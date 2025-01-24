3e18f8dd8d395b4e0b54b9ccb05be9f27caa4bf1
Talicatwolf <rodrigorevestrex@hotmail.com>
2025-01-23 21:25:49 -0300

    menu

--- quizLabyrinth/main_menu.gd
+++ quizLabyrinth/main_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
-
+@onready var start_button: Button = $VBoxContainer/Start_button
+@onready var settings_button: Button = $VBoxContainer/Settings_button
+@onready var options_menu: OptionsMenu = $Options_Menu as OptionsMenu
+@onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
 
 
 func _on_start_pressed() -> void:
@@ -8,4 +11,5 @@ func _on_start_pressed() -> void:
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	margin_container.visible = false
+	options_menu.visible = true
