1ad6a3d8ebdc85a52501a5966daa28ba8ef4ee14
Talicatwolf <rodrigorevestrex@hotmail.com>
2025-01-27 22:03:50 -0300

    Options_working

--- quizLabyrinth/main_menu.gd
+++ quizLabyrinth/main_menu.gd
@@ -5,11 +5,19 @@ extends Control
 @onready var options_menu: OptionsMenu = $Options_Menu as OptionsMenu
 @onready var margin_container: MarginContainer = $MarginContainer as MarginContainer
 
+func _ready() -> void:
+	start_button.button_down.connect(_on_start_pressed)
+	settings_button.button_down.connect(_on_settings_pressed)
+	set_process(false)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://labyrinth.tscn")
 
-
 func _on_settings_pressed() -> void:
-	margin_container.visible = false
+	$VBoxContainer.visible = false
+	options_menu.set_process(true)
 	options_menu.visible = true
+	
+func on_exit_options_menu() -> void:
+	$VBoxContainer.visible = true
+	options_menu.visible = false
